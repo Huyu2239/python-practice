0b6e55ecd5a7d083a82ca92031ea82e9676fdd83
Endo <61175422+Huyu2239@users.noreply.github.com>
2022-05-16 01:20:27 +0000

    add: main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+
+class Mybot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix="./",
+            intents=discord.Intents.all(),
+            allowed_mentions=discord.AllowedMentions(
+                everyone=False, roles=True, users=True
+            ),
+        )
+
+    async def on_ready(self):
+        print("ready")
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    bot = Mybot()
+    bot.run(os.environ["TOKEN"])
